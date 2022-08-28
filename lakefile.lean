8c9715dc994b4d70e12dfd74db05393ba7dca002
Jeremy Avigad <avigad@cmu.edu>
2022-08-28 11:51:45 -0400

    update

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 package LAMR
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git"@"7da24c4024a2cb547d9d6e85943027daa77d850f"
 
 @[defaultTarget]
 lean_lib LAMR
